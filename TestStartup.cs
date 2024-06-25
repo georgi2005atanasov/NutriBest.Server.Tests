9ab691f13f5fe5c1dee859710a5ab425d3cd21dc
georgi2005atanasov <gogida9876@abv.bg>
2024-06-25 21:23:13 +0300

    Configured my tested asp net

---
+++ TestStartup.cs
@@ -0,0 +1,23 @@
+﻿namespace NutriBest.Server.Tests
+{
+    using MyTested.AspNetCore.Mvc;
+
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Configuration;
+
+    public class TestStartup : Startup
+    {
+        public TestStartup(IConfiguration configuration) 
+            : base(configuration)
+        {
+        }
+
+        public void ConfigureTestServices(IServiceCollection services)
+        {
+            base.ConfigureServices(services);
+
+            // Replace only your own custom services. The ASP.NET Core ones 
+            // are already replaced by MyTested.AspNetCore.Mvc.
+        }
+    }
+}
